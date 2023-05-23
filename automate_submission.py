d7921d40691b036c61bcb936e4f3d59a340dd543
AdrienLgl <adrien.laigle35@gmail.com>
2023-05-23 13:13:08 +0200

    get accuracy score from kaggle

--- automate_submission.py
+++ automate_submission.py
@@ -43,5 +43,30 @@ shutil.copy('samples/sample_submission.csv', 'sample_submission.csv')
 # Replace the following line with the command to submit your output file to the Kaggle competition
 os.system('kaggle competitions submit -c spaceship-titanic -f sample_submission.csv -m "Auto submission"')
 
+os.system('kaggle competitions submissions -c spaceship-titanic > score.txt')
+
+f = open("score.txt")
+contents = f.read()
+f.close()
+offsetStart = str.find(contents, 'mmddyyyy-n.csv')
+score = contents[offsetStart+123:offsetStart+130]
+
 # Cleanup files
 os.remove('sample_submission.csv')
+os.remove('score.txt')
+
+print(f'Accuracy score: {score}')
+
+# Save the accuracy score as a GitHub environment variable
+os.environ['ACCURACY_SCORE'] = str(score)
+
+# Update the README badge with the accuracy score
+readme_file = 'README.md'
+badge_url = 'https://img.shields.io/badge/accuracy-{accuracy}-brightgreen'
+
+with open(readme_file, 'r') as f:
+    readme_content = f.read()
+
+updated_readme_content = readme_content.replace('https://img.shields.io/badge/accuracy-', badge_url)
+with open(readme_file, 'w') as f:
+    f.write(updated_readme_content)
