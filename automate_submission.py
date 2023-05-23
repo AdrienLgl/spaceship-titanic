e2db2d7dcf0281bde58641546d9633c1597e5e0e
AdrienLgl <adrien.laigle35@gmail.com>
2023-05-23 12:32:36 +0200

    add json to pipeline

--- automate_submission.py
+++ automate_submission.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import json
 
 # Retrieve Kaggle username and token from GitHub variables
 kaggle_username = os.environ.get('KAGGLE_USERNAME')
@@ -14,6 +15,28 @@ print(files)
 
 # Install Kaggle CLI
 os.system('pip install kaggle')
+
+# Define the path to the .kaggle directory
+kaggle_dir = os.path.expanduser('~/.kaggle')
+
+# Create the .kaggle directory if it doesn't exist
+os.makedirs(kaggle_dir, exist_ok=True)
+
+# Define the path to the kaggle.json file
+kaggle_json_path = os.path.join(kaggle_dir, 'kaggle.json')
+
+# Create a dictionary with the Kaggle credentials
+kaggle_credentials = {
+    'username': kaggle_username,
+    'key': kaggle_token
+}
+
+# Save the Kaggle credentials to the kaggle.json file
+with open(kaggle_json_path, 'w') as f:
+    json.dump(kaggle_credentials, f)
+
+# Set the appropriate permissions for the kaggle.json file
+os.chmod(kaggle_json_path, 0o600)
 
 # Copy sample_submission.csv from your GitHub repository
 shutil.copy('samples/sample_submission.csv', 'sample_submission.csv')
